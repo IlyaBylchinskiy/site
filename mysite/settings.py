41818d82240b58fa68810077e9b0723214f5c36e
Ilya Bylchinskiy <ilya.bylchinskiy@gmail.com>
2022-01-22 14:58:46 +0300

    forms and user accounts

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 EMAIL_DRIVER = smtp
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
